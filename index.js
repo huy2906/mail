b5a86197f3e262746acc7ca95a16c1b2ea7102da
huy2906 <129972351+huy2906@users.noreply.github.com>
2025-11-27 16:55:41 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 import express from "express";
 import cors from "cors";
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Init resend
+const resend = new Resend(process.env.RESEND_API);
+
 app.post("/api/send", async (req, res) => {
   try {
     const { to, subject, text } = req.body;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS
-      }
-    });
-
-    await transporter.sendMail({
-      from: process.env.GMAIL_USER,
+    const data = await resend.emails.send({
+      from: "Mail API <onboarding@resend.dev>",
       to,
       subject,
       text
     });
 
-    res.json({ success: true, message: "Email sent!" });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: error.message });
+    res.json({ success: true, data });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: err.message });
   }
 });
 
 app.get("/", (req, res) => {
-  res.send("Mail API is running!");
+  res.send("Mail API with Resend is running!");
 });
 
 const PORT = process.env.PORT || 10000;
